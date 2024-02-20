47bd4ab567ece751f11aa3c41f93784602623cac
robert.downer <Robert.Downer@fdmgroup.com>
2024-02-20 16:33:22 +0000

    First commit

--- src/components/DeleteExpense.jsx
+++ src/components/DeleteExpense.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import "../styles/DeleteButton.css";
+import "../styles/ExpenseCard.css";
 const DeleteExpense = ({ expenseId, setExpenses }) => {
   const api = `http://localhost:5299/api/Expense/${expenseId}`;
 
