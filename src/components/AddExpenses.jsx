32ba36151637362786a9323f557d739168c8b5d3
robert.downer <Robert.Downer@fdmgroup.com>
2024-02-20 10:40:34 +0000

    Expense post request

--- src/components/AddExpenses.jsx
+++ src/components/AddExpenses.jsx
@@ -3,21 +3,56 @@ import { useEffect, useState } from "react";
 
 const AddExpenses = () => {
   const api = "http://localhost:5299/api/Expense";
+  const apiCategory = "http://localhost:5299/api/ExpenseCategory";
+
+  //Post Request
   const [name, setName] = useState("");
   const [amount, setAmount] = useState("");
   const [category, setCategory] = useState("");
-  const [user, setUser] = useState("");
+
+  //Get Request
+  const [categories, setCategories] = useState([]);
+
+  //Error handling
+  const [nameError, setNameError] = useState(false);
+  const [amountError, setAmountError] = useState(false);
 
   const createExpense = (event) => {
     event.preventDefault();
+    //Stops name containing a number
+    const namePattern = /^[A-Za-z\s]+$/;
+    if (!namePattern.test(name)) {
+      setNameError(true);
+      return; // Don't proceed with creating expense
+    }
 
+    const parsedAmount = parseFloat(amount);
+    if (isNaN(parsedAmount) || parsedAmount < 0) {
+      setAmountError(true);
+      return;
+    }
     axios
-      .post(api, { name: name, amount: amount })
+      .post(api, { name: name, amount: amount, category: category })
       .then((response) => {
         console.log("Expense created:", response.data);
       })
       .catch((error) => console.log(error));
   };
+
+  const loadCategories = () => {
+    axios
+      .get(apiCategory)
+      .then((response) => {
+        setCategories(response.data);
+      })
+      .catch((error) => {
+        console.log("Can't load category data");
+      });
+  };
+
+  useEffect(() => {
+    loadCategories();
+  }, []);
 
   return (
     <div>
@@ -43,7 +78,31 @@ const AddExpenses = () => {
             onChange={(event) => setAmount(event.target.value)}
             required
           />
+          <label htmlFor="category">Category</label>
+          <select
+            id="category"
+            className="form-control"
+            value={category}
+            onChange={(event) => setCategory(event.target.value)}
+            required
+          >
+            <option value="">Select a category</option>
+            {categories.map((category) => (
+              <option key={category.id} value={category.id}>
+                {category.name}
+              </option>
+            ))}
+          </select>
         </div>
+        {nameError && (
+          <div style={{ color: "red" }}>Expense Name cannot have a number.</div>
+        )}
+
+        {amountError && (
+          <div style={{ color: "red" }}>
+            Amount must be positive and a number.
+          </div>
+        )}
         <button type="submit" class="btn btn-primary">
           Add Expense
         </button>
